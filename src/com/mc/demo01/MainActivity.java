4e09a57c18d85f2efe2c04b7eb075e1bc02c68f0
machi <390778636@qq.com>
2014-11-17 12:29:11 +0800

    this is a demo

--- src/com/mc/demo01/MainActivity.java
+++ src/com/mc/demo01/MainActivity.java
@@ -2,6 +2,7 @@ package com.mc.demo01;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("main", "this is a demo");
     }
 
 
